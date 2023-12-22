1ae911af9cfe53b729c3ccfe233137c3c0fa0ea5
Kelly Chen <kellychen966@gmail.com>
2023-12-22 18:56:31 -0500

    Added button to change background color

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -15,5 +15,16 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let random_color = changeColor()
+        view.backgroundColor = random_color
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 }
 
