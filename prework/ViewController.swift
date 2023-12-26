73a5f23f496fa2c192b23dc4a1615e1d6e65db43
Kelly Chen <kellychen966@gmail.com>
2023-12-26 10:39:58 -0500

    trying to change text color

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,7 +18,9 @@ class ViewController: UIViewController {
 
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
         let random_color = changeColor()
+//        let contrast_color = findContrastColor(random_color)
         view.backgroundColor = random_color
+        
     }
     
     func changeColor() -> UIColor {
@@ -25,6 +28,22 @@ class ViewController: UIViewController {
         let green = CGFloat.random(in: 0...1)
         let blue = CGFloat.random(in: 0...1)
         return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+    func findContrastColor(color: UIColor) -> UIColor {
+        var red: CGFloat = 0
+        var blue: CGFloat = 0
+        var green: CGFloat = 0
+        var alpha: CGFloat = 0
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+//        let red = (0.299 * color.red)
+//        let green = (0.587 * color.green)
+//        let blue = (0.114 * color.blue)
+        let luminance = ((0.299 * red) + (0.587 * green) + (0.114 * blue))/255
+//      luminance > 0.5 is bright color
+        let text_color = luminance > 0.5 ? CGFloat(0) : CGFloat(255)
+        return UIColor(red: text_color, green: text_color, blue: text_color, alpha: 0.5)
     }
 }
 
